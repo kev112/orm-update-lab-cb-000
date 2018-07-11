7d0e444bb01b00d3afc0dbfea33bf46d12b71153
Tao <werewaswas@live.com>
2018-07-11 22:12:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,12 +54,13 @@ class Student
   def save
     if self.id
       self.update
-    end
-    sql = SQL <<-
+    else
+      sql = SQL <<-
 
-    SQL
-    row = DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      SQL
+      row = DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
   end
 
 end
