6b51a4fc6aaa5f609b4a65f3fcab4623305834f5
Tao <werewaswas@live.com>
2018-07-11 21:51:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,44 +22,45 @@ class Student
 
   def self.drop_table
     sql = SQL <<-
-      
+
     SQL
     DB[:conn].execute(sql)
   end
 
   def self.create
     sql = SQL <<-
-      
+
     SQL
     DB[:conn].execute(sql)
   end
 
   def self.new_from_db
     sql = SQL <<-
-      
+
     SQL
     DB[:conn].execute(sql)
   end
 
   def self.find_by_name(name)
     sql = SQL <<-
-      
+
     SQL
     DB[:conn].execute(sql, name)
   end
 
   def update
     sql = SQL <<-
-      
+
     SQL
     DB[:conn].execute(sql)
   end
 
   def save
     sql = SQL <<-
-      
+
     SQL
-    DB[:conn].execute(sql)
+    row = DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
-  
+
 end
